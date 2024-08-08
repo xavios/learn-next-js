80b22d37dd3c103f424cba907f7c2440d780bbe8
AndrasHacks <mr.acs.andras@gmail.com>
2024-08-08 23:29:31 +0200

    loading state for meals

---
+++ food-lovers-next-js/app/meals/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./loading.module.css";
+
+const MealsLoading = () => {
+  return <p className={classes.loading}>Loading meals...</p>;
+};
+
+export default MealsLoading;
